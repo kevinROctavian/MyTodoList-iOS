7aea7b2f34adbdee6b711859ee1c5e71ef8c1701
Kevin R. Octavian <kevin.r.octavian@gmail.com>
2023-07-29 09:27:22 +0800

    Initial Commit

---
+++ MyTodolist/MyTodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodolistApp.swift
+//  MyTodolist
+//
+//  Created by Kevin on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MyTodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
